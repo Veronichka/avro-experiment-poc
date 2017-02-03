7d80b194269fd34efef770a2e4b4b43311372944
VHerasymenko <vherasymenko@sciforce.solutions>
2017-02-03 12:33:36 -0500

    * Configured cucmber and created feature file for the acceptance tests.

---
+++ src/test/groovy/com/vherasymenko/avro/inbound/TestSteps.groovy
@@ -0,0 +1,27 @@
+
+package com.vherasymenko.avro.inbound
+
+import com.vherasymenko.avro.Application
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.test.context.ContextConfiguration
+
+/**
+ * Step definitions geared towards Enchantment's acceptance test but remember, all steps are used
+ * by Cucumber unless special care is taken. If you word your features in a consistent manner, then
+ * steps will automatically get reused and you won't have to keep writing the same test code.
+ */
+@WebIntegrationTest( randomPort = true )
+@ContextConfiguration( classes = [Application, AcceptanceTestConfiguration], loader = SpringApplicationContextLoader )
+class TestSteps {
+
+    /**
+     * This is state shared between steps and can be setup and torn down by the hooks.
+     */
+    class MyWorld { }
+
+    /**
+     * Shared between hooks and steps.
+     */
+    MyWorld sharedState
+}
